5c7f3de017580a65c0ecc5f1e17f790a04b34a11
Sebastian Rehm <Sebastian.Rehm@Student.HTW-Berlin.de>
2022-12-30 12:37:11 +0100

    Distance measurement with ioctl, very accurate

--- src/ultrasonic/playground/ioctl/distance.c
+++ src/ultrasonic/playground/ioctl/distance.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <sys/ioctl.h>
+#include <sys/epoll.h>
 #include <linux/gpio.h>
 
 #define TRIGGER 23
@@ -53,12 +54,60 @@ void putPulse(int pin, long microseconds) {
     close(req.fd);
 }
 
+/**
+ * @brief This measures the duration (in microseconds) 
+ * of a high pulse on the given pin.
+ * 
+ * @param pin number 24
+ * @return float microseconds
+ */
+float getPulseUs(int pin) {
+    struct gpioevent_request req;
+    req.lineoffset = pin;
+    req.handleflags = GPIOHANDLE_REQUEST_INPUT;
+    req.eventflags = GPIOEVENT_REQUEST_BOTH_EDGES;
+    strcpy(req.consumer_label, "HC-SR04 Echo");
+
+    int fd = open("/dev/gpiochip0", O_RDONLY);
+    int ret = ioctl(fd, GPIO_GET_LINEEVENT_IOCTL, &req);
+    close(fd);
+
+    static struct epoll_event ev;
+    ev.events = EPOLLIN;
+    ev.data.fd = req.fd;
+    int epfd = epoll_create(1);
+    int res = epoll_ctl(epfd, EPOLL_CTL_ADD, req.fd, &ev);
+
+    long long unsigned start, end;
+    int nfds;
+    
+    for (int i = 0; i < 2; i++)
+    {
+        nfds = epoll_wait(epfd, &ev, 1, 2000);
+        if (nfds != 0)
+        {
+            struct gpioevent_data edata;
+            read(req.fd, &edata, sizeof(edata));
+
+            if (edata.id == 1) // if rising edge
+            {
+                start = edata.timestamp; // nanoseconds
+            }
+            else
+            {
+                end = edata.timestamp; // nanoseconds
+            }
+        }
+    }
+
+    return (float) (end - start) / 1000; // microseconds
+}
+
 int main(void) {
 
-    while(1) {
-        putPulse(23, 5);
-        delayMicroseconds(10000);
-    }
+    putPulse(TRIGGER, 10);
+    float duration = getPulseUs(ECHO);
+    printf("Distance: %.2f cm\n", duration / 58);
 
     return 0;
     
